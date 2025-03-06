583c14629c31da957874bb0f66dd9046caa1a218
B1329025 <B1329025@cgu.edu.tw>
2025-03-06 14:57:25 +0800

    Update main.c

--- Ex1/main.c
+++ Ex1/main.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
-
 int main()
 {
    FILE* fp;
-   fp=fopen("bin","wb+");
+   fp=fopen("a.bin","wb+");
    int a[]={0,1,2,};
    char b[]="ABC";
    float c[]={1.1,1.2,1.3};
@@ -12,15 +10,11 @@ int main()
    float rc[3];
    
    fwrite(a,sizeof(a),1,fp);
+   fwrite(b,sizeof(b),1,fp);
+   fwrite(c,sizeof(c),1,fp);
    fseek(fp,0,SEEK_SET);
    fread(ra,sizeof(ra),1,fp);
-   fseek(fp,0,SEEK_SET);
-   fwrite(b,sizeof(b),1,fp);
-   fseek(fp,0,SEEK_SET);
    fread(rb,sizeof(rb),1,fp);
-   fseek(fp,0,SEEK_SET);
-   fwrite(c,sizeof(a),1,fp);
-   fseek(fp,0,SEEK_SET);
    fread(rc,sizeof(rc),1,fp);
    fclose(fp);
    for(int i=0;i<3;i++){
