f104aa051a5d8f6d92e98de3786f06b4a588f99e
B1329025 <B1329025@cgu.edu.tw>
2025-03-06 15:31:06 +0800

    Update main.c

--- Ex1/main.c
+++ Ex1/main.c
@@ -2,11 +2,11 @@ int main()
 {
    FILE* fp;
    fp=fopen("a.bin","wb+");
-   int a[]={0,1,2,};
+   int a[]={0,1,2};
    char b[]="ABC";
    float c[]={1.1,1.2,1.3};
    int ra[3];
-   char rb[3];
+   char rb[4];
    float rc[3];
    
    fwrite(a,sizeof(a),1,fp);
@@ -16,18 +16,18 @@ int main()
    fread(ra,sizeof(ra),1,fp);
    fread(rb,sizeof(rb),1,fp);
    fread(rc,sizeof(rc),1,fp);
+   for(int i=0;i<3;i++){
+       printf("%d ",ra[i]);
+   }
+   printf("\n");
+   for(int i=0;i<3;i++){
+       printf("%c ",rb[i]);
+   }
+   printf("\n");
+   for(int i=0;i<3;i++){
+       printf("%f ",rc[i]);
+   }
    fclose(fp);
-   for(int i=0;i<3;i++){
-       printf("%d ",a[i]);
-   }
-   printf("\n");
-   for(int i=0;i<3;i++){
-       printf("%c ",b[i]);
-   }
-   printf("\n");
-   for(int i=0;i<3;i++){
-       printf("%f ",c[i]);
-   }
    
   
 }
