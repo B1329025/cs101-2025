2bd41c5d06f0fea1b9dd4b44acf143f48c0aee0a
B1329025 <B1329025@cgu.edu.tw>
2025-05-08 13:42:40 +0800

    Update main.cpp

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class board {
   private:
-  char a[9];
+    char a[9];
   public:
     board(){
         int k=49;
@@ -54,9 +54,10 @@ class board {
         if(a[0]==c && a[4]==c && a[8]==c){
             return true;
         }
-        if(a[2]==c && a[4]==c && a[6]==c){
+        else if(a[2]==c && a[4]==c && a[6]==c){
             return true;
         }
+        else return false;
     }
   
     
@@ -66,17 +67,47 @@ class tic {
     private:
         board board_2;
     public:
-        int player ;
+        int player=0;
+        int n, rounds = 1;
         char symbol ;
         void start(){
-            
+            board_2.print_board();
+            while(rounds!=9){
+                if(player%2==0){
+                    symbol ='O';
+                }
+                else{
+                    symbol='X';
+                }
+                cout<<"player "<<player%2<<" enter 1~9"<<endl;
+                cin>>n;
+                while(true){
+                   if(board_2.check(n,symbol)){
+                       rounds++;
+                       break;
+                   }
+                   else{
+                       cout<<"try again !"<<endl;
+                       cin>>n;
+                   }
+                } 
+                board_2.move(n,symbol);
+                if(board_2.win_lose(symbol)){
+                    cout<<"player "<<player%2<<" win!"<<endl;
+                    break;
+                }
+                player++;
+            }
+            if(!board_2.win_lose(symbol)){
+                cout<<"draw :(";
+            }
         }
     
         
 };
 int main()
 {   
-   board board_1;
-   board_1.print_board();
+   tic tic_1;
+   tic_1.start();
     
 }
